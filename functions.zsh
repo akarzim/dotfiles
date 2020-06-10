89619d5248b955d1d6fc9914b1145287a22fb845
François VANTOMME <akarzim@gmail.com>
2020-06-10 22:18:19 +0200

    Refactor: fix linter warnings

--- functions.zsh
+++ functions.zsh
@@ -1,13 +1,14 @@
+#!/usr/bin/env zsh
 link() {
   local filepath=$argv[1]
   local dotfile=${argv[2]:-.${filepath:t}}
 
-  if [[ $filepath -ef ~/$dotfile ]] then
+  if [[ $filepath -ef ~/$dotfile ]]; then
     echo "$fg[blue][•]$reset_color $dotfile already exists"
-  elif [[ -e ~/$dotfile ]] then
+  elif [[ -e ~/$dotfile ]]; then
     echo "$fg[yellow][?]$reset_color $dotfile exists but point to the wrong file"
-  elif [[ -a $filepath ]] then
-    if ln -sv ${filepath:a} ~/$dotfile; then
+  elif [[ -a $filepath ]]; then
+    if ln -sv "${filepath:a}" "$HOME/$dotfile"; then
       echo "$fg[green][+]$reset_color $dotfile linked"
     else
       echo "$fg[red][!]$reset_color $dotfile link failed"
@@ -21,20 +22,20 @@ copy() {
   local filepath=$argv[1]
   local dotfile=${argv[2]:-.${filepath:t}}
 
-  if [[ -e ~/$dotfile ]] then
-    if diff -q $filepath ~/$dotfile &>/dev/null; then
+  if [[ -e ~/$dotfile ]]; then
+    if diff -q "$filepath" "$HOME/$dotfile" &>/dev/null; then
       echo "$fg[blue][•]$reset_color $dotfile already exists"
     else
       echo "$fg[yellow][?]$reset_color files $filepath and $dotfile differ"
     fi
-  elif [[ -d $filepath ]] then
-    if cp -R ${filepath:a} ~/$dotfile; then
+  elif [[ -d $filepath ]]; then
+    if cp -R "${filepath:a}" "$HOME/$dotfile"; then
       echo "$fg[green][+]$reset_color $dotfile directory copied"
     else
       echo "$fg[red][!]$reset_color $dotfile directory copy failed"
     fi
-  elif [[ -a $filepath ]] then
-    if cp -p ${filepath:a} ~/$dotfile; then
+  elif [[ -a $filepath ]]; then
+    if cp -p "${filepath:a}" "$HOME/$dotfile"; then
       echo "$fg[green][+]$reset_color $dotfile file copied"
     else
       echo "$fg[red][!]$reset_color $dotfile file copy failed"
@@ -48,16 +49,16 @@ decipher() {
   local filepath=$argv[1]
   local dotfile=${argv[2]:-.${filepath:t}}
 
-  if [[ -d $filepath ]] then
+  if [[ -d $filepath ]]; then
     echo "$fg[red][!]$reset_color decipher failed. $dotfile is a directory"
-  elif [[ -e ~/$dotfile ]] then
-    if gpg --no-tty -q -d $filepath | diff -q - ~/$dotfile &>/dev/null; then
+  elif [[ -e ~/$dotfile ]]; then
+    if gpg --no-tty -q -d "$filepath" | diff -q - "$HOME/$dotfile" &>/dev/null; then
       echo "$fg[blue][•]$reset_color $dotfile already exists"
     else
       echo "$fg[yellow][?]$reset_color files $filepath and $dotfile differ"
     fi
-  elif [[ -a $filepath ]] then
-    if gpg --no-tty -q -d $filepath 1> ~/$dotfile; then
+  elif [[ -a $filepath ]]; then
+    if gpg --no-tty -q -d "$filepath" 1> "$HOME/$dotfile"; then
       echo "$fg[green][+]$reset_color $dotfile file deciphered"
     else
       echo "$fg[red][!]$reset_color $dotfile file decipher failed"
--- init.zsh
+++ init.zsh
@@ -1,7 +1,8 @@
+#!/usr/bin/env zsh
 source "functions.zsh"
 
 # zsh
-if [[ -d ${0:A:h:h}/zsh ]] then
+if [[ -d ${0:A:h:h}/zsh ]]; then
   link ${0:A:h:h}/zsh
   copy ${0:A:h:h}/zsh/zlogin
   copy ${0:A:h:h}/zsh/zlogout
@@ -10,13 +11,13 @@ if [[ -d ${0:A:h:h}/zsh ]] then
   copy ${0:A:h:h}/zsh/zshrc
 
   # zplug
-  if [[ -d ~/.zplug ]] then
+  if [[ -d $HOME/.zplug ]]; then
     copy ${0:A:h:h}/zsh/zplug-packages.zsh .zplug/packages.zsh
   fi
 fi
 
 # vim
-if [[ -d ${0:A:h:h}/vim ]] then
+if [[ -d ${0:A:h:h}/vim ]]; then
   link ${0:A:h:h}/vim
   link ${0:A:h:h}/vim .config/nvim
   copy ${0:A:h:h}/vim/config.vim .vimrc
@@ -32,12 +33,12 @@ copy ${0:A:h}/home/tigrc
 copy ${0:A:h}/home/gitconfig
 copy ${0:A:h}/home/gitignore
 
-if [[ -d ~/dev/synbioz ]] then
+if [[ -d $HOME/dev/synbioz ]]; then
   decipher ${0:A:h}/home/dev/synbioz/mailmap.gpg dev/synbioz/.mailmap
   decipher ${0:A:h}/home/dev/synbioz/gitconfig.gpg dev/synbioz/.gitconfig
 fi
 
-if [[ -d ~/dev/perso ]] then
+if [[ -d $HOME/dev/perso ]]; then
   decipher ${0:A:h}/home/dev/perso/mailmap.gpg dev/perso/.mailmap
   decipher ${0:A:h}/home/dev/perso/gitconfig.gpg dev/perso/.gitconfig
 fi
@@ -55,6 +56,6 @@ if which bat &> /dev/null; then
 fi
 
 # Iosevka font
-if [[ -d ~/git/iosevka ]] then
+if [[ -d $HOME/git/iosevka ]]; then
   copy ${0:A:h}/home/fonts/iosevka/private-build-plans.toml private-build-plans.toml
 fi
