ca6b805928136c1a5917ce989391a5b1d2bdf1ed
François VANTOMME <akarzim@gmail.com>
2020-06-21 15:29:38 +0200

    Fix (Iosevka): copy private build plan to the right destination

--- modules/iosevka.zsh
+++ modules/iosevka.zsh
@@ -1,4 +1,4 @@
 # Iosevka font
 if [[ -d $HOME/git/iosevka ]]; then
-  copy ${0:A:h:h}/home/fonts/iosevka/private-build-plans.toml private-build-plans.toml
+  copy ${0:A:h:h}/home/fonts/iosevka/private-build-plans.toml $HOME/git/iosevka/private-build-plans.toml
 fi
