f8e51de9454325694e890b7d12d5ec9494ea56b5
François Vantomme <akarzim@pm.me>
2021-05-24 20:53:08 +0200

    Module (Git): update configuration

--- modules/git.zsh
+++ modules/git.zsh
@@ -1,18 +1,18 @@
 # git
 if program "git"; then
   copy ${0:A:h:h}/home/gitconfig
+  copy ${0:A:h:h}/home/gitconfig-perso
+  copy ${0:A:h:h}/home/gitconfig-synbioz
   copy ${0:A:h:h}/home/gitignore
   copy ${0:A:h:h}/home/gitattributes
 
-  if [[ -d $HOME/dev/synbioz ]]; then
-    rdecipher $HOME/dev/synbioz ${0:A:h:h}/home/dev/synbioz/mailmap.gpg .mailmap
-    rdecipher $HOME/dev/synbioz ${0:A:h:h}/home/dev/synbioz/gitconfig.gpg .gitconfig
-  fi
+  # if [[ -d $HOME/dev/synbioz ]]; then
+  #   rdecipher $HOME/dev/synbioz ${0:A:h:h}/home/dev/synbioz/mailmap.age .mailmap
+  # fi
 
-  if [[ -d $HOME/dev/perso ]]; then
-    rdecipher $HOME/dev/perso ${0:A:h:h}/home/dev/perso/mailmap.gpg .mailmap
-    rdecipher $HOME/dev/perso ${0:A:h:h}/home/dev/perso/gitconfig.gpg .gitconfig
-  fi
+  # if [[ -d $HOME/dev/perso ]]; then
+  #   rdecipher $HOME/dev/perso ${0:A:h:h}/home/dev/perso/mailmap.age .mailmap
+  # fi
 else
   skip "git"
 fi
