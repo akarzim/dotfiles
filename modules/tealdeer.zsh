c29394c5e44edf9922ffbdee300f2be0d6ea425f
François Vantomme <akarzim@pm.me>
2022-03-15 15:40:23 +0100

    refactor(tldr): use --show-paths option

--- modules/tealdeer.zsh
+++ modules/tealdeer.zsh
@@ -1,6 +1,6 @@
 # tealdeer
 if program "tldr"; then
-  copy ${0:A:h:h}/home/config/tealdeer.toml "`tldr --config-path | awk -F ': ' '{print $2}'`"
+  copy ${0:A:h:h}/home/config/tealdeer.toml "`tldr --show-paths | awk '/Config path/ {print $3}'`"
 else
   skip "tldr"
 fi
