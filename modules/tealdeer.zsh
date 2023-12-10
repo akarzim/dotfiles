b70338cf05c915c139b3389e0ea6728dd4b9570b
François Vantomme <francois.vantomme@enercoop.org>
2023-12-10 12:20:33 +0100

    fix(tldr): skip config unless tealdeer client is in use

--- modules/tealdeer.zsh
+++ modules/tealdeer.zsh
@@ -1,5 +1,5 @@
 # tealdeer
-if program "tldr"; then
+if program "tldr" && (tldr -v | grep -q 'tealdeer'); then
   copy ${0:A:h:h}/home/config/tealdeer.toml "`tldr --show-paths | awk '/Config path/ {print $3}'`"
 else
   skip "tldr"
