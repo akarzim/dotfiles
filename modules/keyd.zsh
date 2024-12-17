4dce0998cf15a2b5314eba32ce29e3fc4efee9cc
François Vantomme <francois.vantomme@enercoop.org>
2024-12-17 07:26:36 +0100

    feat(keyd): configure prev/next mouse buttons

--- modules/keyd.zsh
+++ modules/keyd.zsh
@@ -3,6 +3,8 @@ if program "keyd"; then
   scopy ${0:A:h:h}/etc/keyd/default.conf /etc/keyd/default.conf
   scopy ${0:A:h:h}/etc/keyd/mx_master.conf /etc/keyd/mx_master.conf
   scopy ${0:A:h:h}/etc/keyd/typematrix.conf /etc/keyd/typematrix.conf
+  notify "keyd reload"
+  sudo keyd reload
 else
   skip "keyd"
 fi
