44c789f497b9ffc9f8091ab7cb623f18ba20000b
François Vantomme <akarzim@pm.me>
2025-05-03 00:54:30 +0200

    feat(git): improve configuration

--- modules/git.zsh
+++ modules/git.zsh
@@ -1,6 +1,7 @@
 # git
 if program "git"; then
   copy ${0:A:h:h}/home/gitconfig
+  copy ${0:A:h:h}/home/gitconfig-enercoop
   copy ${0:A:h:h}/home/gitconfig-perso
   copy ${0:A:h:h}/home/gitignore
   copy ${0:A:h:h}/home/gitattributes
