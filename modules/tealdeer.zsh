9fbcf38800eab1361556528f102f59281839b9e7
François Vantomme <akarzim@pm.me>
2025-05-03 00:59:13 +0200

    feat(tldr): add an alias

--- modules/tealdeer.zsh
+++ modules/tealdeer.zsh
@@ -1,6 +1,6 @@
 # tealdeer
-if program "tldr" && (tldr -v | grep -q 'tealdeer'); then
+if program "tldr" "tealdeer" && (tldr -v | grep -q 'tealdeer'); then
   copy ${0:A:h:h}/home/config/tealdeer.toml "`tldr --show-paths | awk '/Config path/ {print $3}'`"
 else
-  skip "tldr"
+  skip "tealdeer"
 fi
