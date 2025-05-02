928bb42aa0e78300ce2250bc22e24867cfb000a3
François Vantomme <akarzim@pm.me>
2025-05-03 01:02:05 +0200

    feat(vim): use a dedicated repo for nvim

--- modules/nvim.zsh
+++ modules/nvim.zsh
@@ -1,6 +1,30 @@
 # nvim
-if program "nvim" && [[ -d ${0:A:h:h:h}/vim ]]; then
-  link ${0:A:h:h:h}/vim .config/nvim
+if program "nvim"; then
+  if [[ ! -d ${XDG_CONFIG_HOME:-$HOME/.config}/nvim ]]; then
+    notify "please fetch nvim configuration by running:"
+    notify "  git clone git@github.com:akarzim/kickstart.nvim.git ${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
+    notify "  git switch config"
+  elif [[ ! -d ${XDG_CONFIG_HOME:-$HOME/.config}/nvim/.git ]]; then
+    notify "git repository not found"
+  else
+    pushd ${XDG_CONFIG_HOME:-$HOME/.config}/nvim
+    if (git diff --exit-code --quiet); then
+      info "nvim configuration is up to date!"
+    else
+      notify "consider versioning your recent modifications!"
+      if (( $DIFF )); then
+        if (( $INTERACTIVE )) && (( $+commands[tig] )); then
+          tig status
+          if (git diff --exit-code --quiet); then
+            info "nvim configuration is up to date!"
+          fi
+        else
+          git diff --no-ext-diff
+        fi
+      fi
+    fi
+    popd
+  fi
 else
   skip "nvim"
 fi
--- modules/vim.zsh
+++ modules/vim.zsh
@@ -1,7 +1,31 @@
 # vim
-if program "vim" && [[ -d ${0:A:h:h:h}/vim ]]; then
-  link ${0:A:h:h:h}/vim
-  copy ${0:A:h:h:h}/vim/config.vim .vimrc
+if program "vim"; then
+  if [[ ! -d ${XDG_CONFIG_HOME:-$HOME/.config}/vim ]]; then
+    notify "please fetch vim configuration by running:"
+    notify "  git clone git@github.com:akarzim/.vim.git ${XDG_CONFIG_HOME:-$HOME/.config}/vim"
+    notify "  git switch config"
+  elif [[ ! -d ${XDG_CONFIG_HOME:-$HOME/.config}/vim/.git ]]; then
+    notify "git repository not found"
+  else
+    copy ${XDG_CONFIG_HOME:-$HOME/.config}/vim/config.vim .vimrc
+    pushd ${XDG_CONFIG_HOME:-$HOME/.config}/vim
+    if (git diff --exit-code --quiet); then
+      info "vim configuration is up to date!"
+    else
+      notify "consider versioning your recent modifications!"
+      if (( $DIFF )); then
+        if (( $INTERACTIVE )) && (( $+commands[tig] )); then
+          tig status
+          if (git diff --exit-code --quiet); then
+            info "vim configuration is up to date!"
+          fi
+        else
+          git diff --no-ext-diff
+        fi
+      fi
+    fi
+    popd
+  fi
 else
   skip "vim"
 fi
