09e9761991deaf96d6e95fa92d81f1e00223e03a
François Vantomme <akarzim@pm.me>
2025-05-03 02:29:40 +0200

    feat(zsh): improve external git repo support

--- modules/zsh.zsh
+++ modules/zsh.zsh
@@ -1,13 +1,39 @@
 # zsh
-if program "zsh" && [[ -d ${0:A:h:h:h}/zsh ]]; then
-  link ${0:A:h:h:h}/zsh
-  copy ${0:A:h:h:h}/zsh/zfunctions
-  copy ${0:A:h:h:h}/zsh/zlogin
-  copy ${0:A:h:h:h}/zsh/zlogout
-  copy ${0:A:h:h:h}/zsh/zprofile
-  copy ${0:A:h:h:h}/zsh/zshenv
-  copy ${0:A:h:h:h}/zsh/zshrc
-  decipher ${0:A:h:h}/home/zsecrets.age .zsecrets
+if program "zsh"; then
+  if [[ ! -d ${0:A:h:h:h}/zsh ]]; then
+    notify "please fetch zsh configuration by running:"
+    notify "  git clone git@github.com:akarzim/.zsh.git ${0:A:h:h:h}/zsh"
+    notify "  git switch config"
+  elif [[ ! -d ${0:A:h:h:h}/zsh/.git ]]; then
+    notify "git repository not found"
+  else
+    link ${0:A:h:h:h}/zsh
+    link ${0:A:h:h:h}/zsh/zfunctions
+    link ${0:A:h:h:h}/zsh/zlogin
+    link ${0:A:h:h:h}/zsh/zlogout
+    link ${0:A:h:h:h}/zsh/zprofile
+    link ${0:A:h:h:h}/zsh/zshenv
+    link ${0:A:h:h:h}/zsh/zshrc
+    decipher ${0:A:h:h}/home/zsecrets.age .zsecrets
+
+    pushd ${0:A:h:h:h}/zsh
+    if (git diff --exit-code --quiet); then
+      info "zsh configuration is up to date!"
+    else
+      notify "consider versioning your recent modifications!"
+      if (( $DIFF )); then
+        if (( $INTERACTIVE )) && (( $+commands[tig] )); then
+          tig status
+          if (git diff --exit-code --quiet); then
+            info "zsh configuration is up to date!"
+          fi
+        else
+          git diff --no-ext-diff
+        fi
+      fi
+    fi
+    popd
+  fi
 
   # zplug
   if [[ -d $HOME/.zplug ]]; then
